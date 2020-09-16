6ce7352d4438a897eb38897a065b95681ebdd636
eric5605 <eah7279@live.com>
2020-09-16 19:59:21 -0400

    added new baackground, added event listener for user input, added random number generator

--- js/application.js
+++ js/application.js
@@ -1,3 +1,14 @@
-$(document).ready(function () {
-  console.log('it works');
+$(document).ready(function(){
+
+  $('#user-input').on('keyup', function () {
+    console.log($(this).val());
+  });
+
+  var randonNumberGenerator = function (num) {
+    console.log(Math.floor((Math.random() * num) + 1));
+  }
+
+
+  randonNumberGenerator(10);
+
 });
