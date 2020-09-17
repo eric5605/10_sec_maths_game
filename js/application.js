be0f08d7386faafd523e9de62e0bdd632f513a61
eric5605 <eah7279@live.com>
2020-09-17 13:31:31 -0400

    added addition equation, function to determine if user answer equals maths answer

--- js/application.js
+++ js/application.js
@@ -1,14 +1,43 @@
 $(document).ready(function(){
 
-  $('#user-input').on('keyup', function () {
-    console.log($(this).val());
-  });
+var userAnswer;
+var mathsAnswer;
 
-  var randonNumberGenerator = function (num) {
-    console.log(Math.floor((Math.random() * num) + 1));
+
+  var randomNumberGenerator = function (num) {
+    // console.log(Math.floor((Math.random() * num) + 1));
+    return Math.floor((Math.random() * num) + 1);
   }
 
+  var createMathsQuestion = function () {
 
-  randonNumberGenerator(10);
+    var num1 = randomNumberGenerator(10);
+    var num2 = randomNumberGenerator(10);
+    mathsAnswer = num1 + num2;
+     console.log(String(num1), ' + ', String(num2), ' = ');
+     $('#equation').text(String(num1) + " + " + String(num2) + " = ");
 
+  }
+
+  var checkUserInput = function () {
+    return (userAnswer === mathsAnswer);
+  }
+
+  var gamePlay = function () {
+    createMathsQuestion();
+
+    $('#user-input').on('keyup', function () {
+      // console.log($(this).val());
+      userAnswer = Number($(this).val());
+      console.log('userAnswer: ', userAnswer);
+      console.log('mathsAnswer: ', mathsAnswer);
+
+      if(checkUserInput()) {
+        console.log('add one sec');
+      }
+    });
+
+  }
+
+   gamePlay();
 });
