32e4b0991063d3bfbbbea5d836252cc7aef36d37
eric5605 <eah7279@live.com>
2020-09-17 16:09:21 -0400

    updated countdown clock, added function to increase timer by 1, added function to create new equation

--- js/application.js
+++ js/application.js
@@ -2,39 +2,50 @@ $(document).ready(function(){
 
 var userAnswer;
 var mathsAnswer;
-
+var timeLeft = 10;
 
   var randomNumberGenerator = function (num) {
-    // console.log(Math.floor((Math.random() * num) + 1));
     return Math.floor((Math.random() * num) + 1);
   }
 
   var createMathsQuestion = function () {
-
     var num1 = randomNumberGenerator(10);
     var num2 = randomNumberGenerator(10);
     mathsAnswer = num1 + num2;
      console.log(String(num1), ' + ', String(num2), ' = ');
      $('#equation').text(String(num1) + " + " + String(num2) + " = ");
-
   }
 
-  var checkUserInput = function () {
-    return (userAnswer === mathsAnswer);
+  var checkUserInput = function (userInput) {
+    return (userInput === mathsAnswer);
   }
+
+  var updateTimer = function (amount) {
+    timeLeft += amount;
+    $('#countdown').text(timeLeft);
+  }
+
+  //  timer/countdown
+  var interval = setInterval(function () {
+    updateTimer(-1);
+    if (timeLeft === 0) {
+      clearInterval(interval);
+    }
+  }, 1000);
 
   var gamePlay = function () {
     createMathsQuestion();
 
     $('#user-input').on('keyup', function () {
-      // console.log($(this).val());
       userAnswer = Number($(this).val());
       console.log('userAnswer: ', userAnswer);
       console.log('mathsAnswer: ', mathsAnswer);
-
-      if(checkUserInput()) {
-        console.log('add one sec');
+      if (checkUserInput(userAnswer)) {
+        updateTimer(1);
+        createMathsQuestion();
       }
+
+
     });
 
   }
