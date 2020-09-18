6caa152cc9cb21ef87e03dd73ccd91409a8e498e
eric5605 <eah7279@live.com>
2020-09-18 16:33:21 -0400

    finished basic requirements for game

--- js/application.js
+++ js/application.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 var userAnswer;
 var mathsAnswer;
 var timeLeft = 10;
+var numberOfTurns = 0;
 
   var randomNumberGenerator = function (num) {
     return Math.floor((Math.random() * num) + 1);
@@ -13,7 +14,6 @@ var timeLeft = 10;
     var num2 = randomNumberGenerator(10);
     mathsAnswer = num1 + num2;
      console.log(String(num1), ' + ', String(num2), ' = ');
-     // $('#equation').text(String(num1) + " + " + String(num2) + " = ");
       $('#equation').text(String(num1) + " + " + String(num2));
   }
 
@@ -25,37 +25,52 @@ var timeLeft = 10;
     timeLeft += amount;
     $('#countdown').text(timeLeft);
   }
+  var resetGame = function () {
+    timeLeft = 10;
+    numberOfTurns = 0;
+  }
 
   //  timer/countdown
-  // var timeleft = 10;
-    var downloadTimer = setInterval(function(){
+  function countdownTimer () {
+    var countdownTimer = setInterval(function(){
       if(timeLeft <= 0){
-        clearInterval(downloadTimer);
+        clearInterval(countdownTimer);
         document.getElementById("countdown").innerHTML = "Game Over!";
+        resetGame();
       } else {
         document.getElementById("countdown").innerHTML = timeLeft;
       }
       timeLeft -= 1;
     }, 1000);
+  }
 
-  var gamePlay = function () {
+  var playGame = function () {
     createMathsQuestion();
-
+    // get user input
     $('#user-input').on('keyup', function () {
+      if (numberOfTurns === 0) {
+        numberOfTurns++;
+        countdownTimer();
+      }
       userAnswer = Number($(this).val());
-      console.log('userAnswer: ', userAnswer);
-      console.log('mathsAnswer: ', mathsAnswer);
+
+      // console.log('userAnswer: ', userAnswer);
+      // console.log('mathsAnswer: ', mathsAnswer);
 
       if (checkUserInput(userAnswer)) {
         $('#user-input').val('');
         updateTimer(1);
         createMathsQuestion();
       }
-
-
     });
-
   }
 
-   gamePlay();
+  // $('#user-input').on('keyup', function () {
+  //
+  //   userAnswer = Number($(this).val());
+  //   console.log('userAnswer: ', userAnswer);
+  //   console.log('mathsAnswer: ', mathsAnswer);
+  // });
+
+  playGame();
 });
